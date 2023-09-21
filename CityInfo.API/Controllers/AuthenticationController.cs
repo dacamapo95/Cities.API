58634786ec12332279b6c5d723e59d38fbacf269
dacamapo <dacamapo>
2023-09-20 21:00:18 -0500

    Add AuthenticationController

---
+++ CityInfo.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using CityInfo.API.Entities.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : Controller
+    {
+        [HttpPost("authenticate")]
+        public ActionResult<string> Authenticate(
+            AuthenticationRequestBody authenticationRequestBody)
+        {
+
+        }
+    }
+}
---
+++ CityInfo.API/Entities/Security/AuthenticationRequestBody.cs
@@ -0,0 +1,9 @@
+﻿namespace CityInfo.API.Entities.Security
+{
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
